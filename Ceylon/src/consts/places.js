1167a0c2e9bd962cf120bc47b3770543c70e2cbb
Thanura Navodya <59390864+ThanuraN3@users.noreply.github.com>
2021-10-25 00:36:20 +0530

    2021.10.24 modified this app

---
+++ Ceylon/src/consts/places.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-unused-vars
+const places = [
+  {
+    id: '1',
+    name: 'Sigiriya Rock',
+    location: 'Sigiriya',
+    Image: require('../assets/sigiriya.jpg'),
+    details:
+      'Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka. The name refers to a site of historical and archaeological significance that is dominated by a massive column of rock around 180 metres (590 ft) high.',
+  },
+  {
+    id: '2',
+    name: 'Udawalawe National Park',
+    location: 'Udawalawe',
+    Image: require('../assets/udawalawe.jpg'),
+    details:
+      'Udawalawe National Park lies on the boundary of Sabaragamuwa and Uva Provinces, in Sri Lanka. The national park was created to provide a sanctuary for wild animals displaced by the construction of the Udawalawe Reservoir on the Walawe River, as well as to protect the catchment of the reservoir. The reserve covers 30,821 hectares (119.00 sq mi) of land area and was established on 30 June 1972.',
+  },
+  {
+    id: '3',
+    name: 'Galle Dutch Fort',
+    location: 'Galle',
+    Image: require('../assets/galle fort.jpg'),
+    details:
+      'Galle Fort in the Bay of Galle on the southwest coast of Sri Lanka, was built first in 1588 by the Portuguese, then extensively fortified by the Dutch during the 17th century from 1649 onwards. It is a historical, archaeological and architectural heritage monument, which even after more than 432 years maintains a polished appearance, due to extensive reconstruction work done by Archaeological Department of Sri Lanka',
+  },
+  {
+    id: '4',
+    name: 'Nine arch',
+    location: 'Ella',
+    Image: require('../assets/ninearch.jpg'),
+    details:
+      'The Nine Arch Bridge also called the Bridge in the Sky, Nine arch is a viaduct bridge in Sri Lanka. It is one of the best examples of colonial-era railway construction in the country. Nine arch The construction of the bridge is generally attributed to a local Ceylonese builder, P. K. Appuhami, in consultation with British engineers. The chief designer and project manager of the upcountry railway line of Ceylon project was D. J. Wimalasurendra, a distinguished Ceylonese engineer and inventor.',
+  },
+];
---
+++ Ceylon/src/view/screen/DetailsScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../../consts/colors';
+
+const detailsScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+const style = StyleSheet.create({});
+
+export default detailsScreen;
